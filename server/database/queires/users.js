4621b7c0ce60572887b54781794acdcef0ec3d7a
unknown <stm1998@hotmail.com>
2022-03-02 05:37:49 +0200

    create and compleate the user quierys relates to 16-18-19 Relates #16

--- server/database/queires/users.js
+++ server/database/queires/users.js
@@ -1,8 +1,47 @@
 const connection = require('../config/connection');
-
+// insert to users
 const store = (name, password) => connection.query({
   text: 'INSERT INTO users (name, password) VALUES ($1, $2);',
   values: [name, password],
 });
 
-module.exports = { store };
+// select all users to test perposes
+const selectAllUsers = () => connection.query({
+  text: 'SELECT id,name FROM users',
+});
+
+// select user with password
+const selectUserByIdWithPassword = (id) => connection.query({
+  text: 'SELECT * FROM users WHERE id = ($1)',
+  values: [id],
+});
+
+// select user without get password
+const selectUserById = (id) => connection.query({
+  text: 'SELECT id,name FROM users WHERE id = ($1)',
+  values: [id],
+});
+
+// updata user if you have id
+const updateUserById = (id, name, password) => connection.query({
+  text: `UPDATE users 
+    SET name = ($1),
+    password = ($2)
+    WHERE id = ($3);`,
+  values: [name, password, id],
+});
+
+// delete user with id
+const deleteUser = (id) => connection.query({
+  text: 'DELETE FROM users WHERE id = ($1);',
+  values: [id],
+});
+
+module.exports = {
+  store,
+  selectAllUsers,
+  updateUserById,
+  selectUserByIdWithPassword,
+  selectUserById,
+  deleteUser,
+};
