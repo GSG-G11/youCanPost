953e6946cf8ed289f8fc790f29ed0c9bdcbc0c3d
unknown <stm1998@hotmail.com>
2022-03-02 06:19:15 +0200

    create user controller and router relates 28 Relates #28

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
-const router = require('./routes');
+const router = require('./controllers/userController');
 
 const filePath = path.join(__dirname, '..', '..', 'public');
 
